d0a45d9650a627c867217c69944ee675544edc1b
Yug polara <niharpolara9@gmail.com>
2025-10-15 18:31:41 +0530

    completed signIn-signUp form

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,14 @@
-import React from 'react'
+import React from "react";
 
 const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <div>
+      <img
+        src="https://images.ctfassets.net/4cd45et68cgf/Rx83JoRDMkYNlMC9MKzcB/2b14d5a59fc3937afd3f03191e19502d/Netflix-Symbol.png?w=700&h=456"
+        alt="Logo"
+      />
+    </div>
+  );
+};
 
-export default Header
+export default Header;
